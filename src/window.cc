59e9f82c9eb61f1a8025fe06053c84d35ec21132
v4m3rrr <igorurbanowicz@gmail.com>
2025-11-29 16:47:50 +0100

    Window fix and lsan suppresions

--- src/window.cc
+++ src/window.cc
@@ -65,6 +65,7 @@ namespace piksel
 
   Window::~Window()
   {
+    glfwDestroyWindow(p_window_);
     glfwTerminate();
   }
 }
---
+++ tests/mem_test.cc
@@ -0,0 +1,16 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void test()
+{
+
+  glfwInit();
+  //GLFWwindow *wnd=glfwCreateWindow(500,500,"chuj",NULL,NULL);
+  //glfwDestroyWindow(wnd);
+  glfwTerminate();
+}
+
+int main()
+{
+  test();
+}

64a331c72465ac39d2866ef01c70ed6144140baa
v4m3rrr <igorurbanowicz@gmail.com>
2026-05-10 21:35:25 +0200

    Delete all files

--- src/app.cc
+++
@@ -1,9 +0,0 @@
-#include "piksel/app.hh"
-
-namespace piksel
-{
-  App::App(std::string_view title, int width, int height)
-    :wnd(title.data(),width,height),gfx(wnd)
-  {
-  }
-}
--- src/color.cc
+++
@@ -1,9 +0,0 @@
-#include "piksel/color.hh"
-
-namespace piksel {
-	const Color Color::Red{ glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) };
-	const Color Color::Green{ glm::vec4(0.0f, 1.0f, 0.0f, 1.0f) };
-	const Color Color::Blue{ glm::vec4(0.0f, 0.0f, 1.0f, 1.0f) };
-	const Color Color::Black{ glm::vec4(0.0f, 0.0f, 0.0f, 1.0f) };
-	const Color Color::White{ glm::vec4(1.0f, 1.0f, 1.0f, 1.0f) };
-}
